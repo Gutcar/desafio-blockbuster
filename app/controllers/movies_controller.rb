82bb0737c48e04caee52f44a784260508103c3a4
Gutcar <cecicg96@gmail.com>
2023-05-26 08:23:48 -0400

    cambiado mensajes notice

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,7 +26,7 @@ class MoviesController < ApplicationController
 
     respond_to do |format|
       if @movie.save
-        format.html { redirect_to movie_url(@movie), notice: "Movie was successfully created." }
+        format.html { redirect_to movie_url(@movie), notice: "Creado exitosamente" }
         format.json { render :show, status: :created, location: @movie }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +39,7 @@ class MoviesController < ApplicationController
   def update
     respond_to do |format|
       if @movie.update(movie_params)
-        format.html { redirect_to movie_url(@movie), notice: "Movie was successfully updated." }
+        format.html { redirect_to movie_url(@movie), notice: "Editado exitosamente" }
         format.json { render :show, status: :ok, location: @movie }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,7 +53,7 @@ class MoviesController < ApplicationController
     @movie.destroy
 
     respond_to do |format|
-      format.html { redirect_to movies_url, notice: "Movie was successfully destroyed." }
+      format.html { redirect_to movies_url, notice: "Eliminado exitosamente" }
       format.json { head :no_content }
     end
   end
